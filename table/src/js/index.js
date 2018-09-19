a70a0036b661fd7b4364231aaf338e22fa88595a
msminsu <msminsu@gmail.com>
2018-09-20 00:20:06 +0900

    page1

--- table/src/js/index.js
+++ table/src/js/index.js
@@ -1,13 +1,42 @@
 $(document).ready( function() {
-    $( "#datepicker1" ).datepicker({
+    console.log(10)
+    $( "#datepicker-1" ).datepicker({
 		changeMonth: true,
 		changeYear: true,
 		showMonthAfterYear: true,
 		dateFormat: "yy-mm-dd",
 		monthNamesShort: ['1','2','3','4','5','6','7','8','9','10','11','12'],
-		
+		onSelect: function(date) {
+			var s_date = $("#datepicker-2").val() ;
+			var d1 = new Date(date) ;
+			var d2 = new Date(s_date) ;
+			d1.setHours(0, 0, 0, 0) ;
+			d2.setHours(0, 0, 0, 0) ;
+			if (d1 > d2) {
+				$("#datepicker-2").val(date) ;
+			}
+		}
+	});
+	$( "#datepicker-2" ).datepicker({
+		changeMonth: true,
+		changeYear: true,
+		showMonthAfterYear: true,
+		dateFormat: "yy-mm-dd",
+		monthNamesShort: ['1','2','3','4','5','6','7','8','9','10','11','12'],
+		beforeShowDay: function(date) {
+			var s_date = $("#datepicker-1").val() ;
+			var d1 = new Date(date) ;
+			var d2 = new Date(s_date) ;
+			d1.setHours(0, 0, 0, 0) ;
+			d2.setHours(0, 0, 0, 0) ;
+			if (d1 < d2) {
+				return [false] ;
+			} else {
+				return [true] ;
+			}
+		}
     });
-
+    
     $('.btn1').on('click',open_win);
 
     $('.btn2').on('click',function(){
